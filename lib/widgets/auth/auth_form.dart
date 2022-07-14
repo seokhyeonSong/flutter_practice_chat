a98f65312d180548900bc534af97dafb796ed228
seokhyeon12345@gmail.com <seokhyeon12345@gmail.com>
2022-07-14 21:17:56 +0900

    ADD : add validation & mode change at auth screen add validation on textfield and add mode switching

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -8,6 +8,22 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    if (_formkey.currentState == null) return;
+    final isValid = _formkey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formkey.currentState!.save();
+      // User those values to send our auth request ...
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,35 +33,77 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: _formkey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value == null)
+                        return 'Please enter a valid email address';
+                      if (value.isEmpty || !value.contains('@'))
+                        return 'Please enter a valid email address';
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: const InputDecoration(
                       labelText: "Email address",
                     ),
+                    onSaved: (value) {
+                      if (value != null) _userEmail = value;
+                    },
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Username',
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value == null)
+                          return 'Please enter at least 4 characters.';
+                        if (value.isEmpty || value.length < 4)
+                          return 'Please enter at least 4 characters.';
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                      ),
+                      onSaved: (value) {
+                        if (value != null) _userName = value;
+                      },
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value == null)
+                        return 'Password must be at least 7 characters long.';
+                      if (value.isEmpty || value.length < 7)
+                        return 'Password must be at least 7 characters long.';
+                      return null;
+                    },
                     decoration: const InputDecoration(
                       labelText: 'Password',
                     ),
+                    onSaved: (value) {
+                      if (value != null) _userPassword = value;
+                    },
                   ),
                   const SizedBox(
                     height: 12,
                   ),
                   RaisedButton(
-                    onPressed: () {},
-                    child: const Text('Login'),
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'Sign up'),
                   ),
                   FlatButton(
-                    onPressed: () {},
-                    child: const Text("Create new account"),
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(_isLogin
+                        ? "Create new account"
+                        : "i already have an account"),
                   )
                 ],
               ),
